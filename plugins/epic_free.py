e4821402cbf201afc394033d8672b10c170497ec
su226 <thesu226@outlook.com>
2022-10-24 18:12:11 +0800

    优化EPIC喜加一功能

--- plugins/epic_free.py
+++ plugins/epic_free.py
@@ -1,20 +1,30 @@
-from datetime import datetime, timedelta, timezone
+from dataclasses import dataclass
+from datetime import datetime, timezone
 from typing import Iterable
 
-from aiohttp import ClientSession
-from nonebot.adapters.onebot.v11 import Message
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
 
-from util import command
+from util import command, util
 
 API = (
-  "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN"
-  "&country=CN&allowCountries=CN")
+  "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions"
+  "?locale=zh-CN&country=CN&allowCountries=CN"
+)
+URL_BASE = "https://www.epicgames.com/store/zh-CN/p/"
 FREE = {"discountType": "PERCENTAGE", "discountPercentage": 0}
-ZERO = timedelta()
 
 
-def iter_promotions(promotions: dict) -> Iterable[dict]:
-  if promotions is None:
+@dataclass
+class Game:
+  start_date: datetime
+  end_date: datetime
+  title: str
+  image: str
+  slug: str
+
+
+def promotions(promotions: dict) -> Iterable[dict]:
+  if not promotions:
     return
   for i in promotions["promotionalOffers"]:
     yield from i["promotionalOffers"]
@@ -22,58 +32,63 @@ def iter_promotions(promotions: dict) -> Iterable[dict]:
     yield from i["promotionalOffers"]
 
 
-def getslug(game: dict) -> str | None:
-  slug = game['productSlug']
+def getslug(game: dict) -> str:
+  slug = game["productSlug"]
   if slug and slug != "[]":
     return slug.removesuffix("/home")
   for i in game["offerMappings"]:
-    if i.get("pageType", None) == "productHome":
+    if i.get("pageType", "") == "productHome":
       return i["pageSlug"]
-  return None
+  return ""
 
 
-def iter_free_games(games: list[dict]) -> Iterable[str]:
+def getimage(game: dict) -> str:
+  for i in game["keyImages"]:
+    if i["type"] in ("DieselStoreFrontWide", "OfferImageWide"):
+      return i["url"]
+  return ""
+
+
+def free_games(games: list[dict]) -> Iterable[Game]:
   now_date = datetime.now(timezone.utc)
   for game in games:
-    title = game["title"]
-    slug = getslug(game)
-    url = "链接未知" if not slug else f"https://www.epicgames.com/store/zh-CN/p/{slug}"
-    image = ""
-    for i in game["keyImages"]:
-      if i["type"] in ("DieselStoreFrontWide", "OfferImageWide"):
-        image = i["url"]
-        break
-    for i in iter_promotions(game.get("promotions", None)):
+    for i in promotions(game.get("promotions", {})):
       # Python不支持Z结束，须替换成+00:00
       start_date = datetime.fromisoformat(i["startDate"].replace("Z", "+00:00"))
-      start_date_str = start_date.astimezone().strftime("%Y-%m-%d %H:%M:%S")
       end_date = datetime.fromisoformat(i["endDate"].replace("Z", "+00:00"))
-      end_date_str = end_date.astimezone().strftime("%Y-%m-%d %H:%M:%S")
-      if i["discountSetting"] == FREE and end_date - start_date > ZERO:
-        if start_date < now_date < end_date:
-          yield f"[CQ:image,file={image}]{title} 目前免费，截止到 {end_date_str}\n{url}"
-          break
-        elif now_date < start_date:
-          yield f"[CQ:image,file={image}]{title} 将在 {start_date_str} 免费，截止到 {end_date_str}\n{url}"
-          break
+      if i["discountSetting"] == FREE and start_date < end_date and now_date < end_date:
+        yield Game(start_date, end_date, game["title"], getimage(game), getslug(game))
+        break
 
 
-epicfree = (
-  command.CommandBuilder("epicfree", "epicfree", "epic", "喜加一")
-  .brief("看看E宝又在送什么")
+epicfree = command.CommandBuilder("epicfree", "epicfree", "epic", "e宝", "喜加一") \
+  .brief("看看E宝又在送什么") \
   .usage('''\
 /epicfree - 查看现在的免费游戏
-你送游戏你是我宝，你卖游戏翻脸不认（雾）''')
-  .build())
-
-
+你送游戏你是我宝，你卖游戏翻脸不认（雾）''') \
+  .build()
 @epicfree.handle()
 async def handle_epicfree():
-  async with ClientSession() as http:
-    response = await http.get(API)
+  http = util.http()
+  async with http.get(API) as response:
     data = await response.json()
-  result = "\n".join(iter_free_games(data["data"]["Catalog"]["searchStore"]["elements"]))
-  if not result:
+  games = list(free_games(data["data"]["Catalog"]["searchStore"]["elements"]))
+  if not games:
     await epicfree.finish("似乎没有可白嫖的游戏")
-  else:
-    await epicfree.finish(Message(result))
+  games.sort(key=lambda x: x.end_date)
+  now_date = datetime.now(timezone.utc)
+  message = Message()
+  for game in games:
+    end_str = game.end_date.astimezone().strftime("%Y-%m-%d %H:%M")
+    if now_date > game.start_date:
+      text = f"{game.title} 目前免费，截止到 {end_str}"
+    else:
+      start_str = game.start_date.astimezone().strftime("%Y-%m-%d %H:%M")
+      text = f"{game.title} 将在 {start_str} 免费，截止到 {end_str}"
+    if message:
+      text = "\n" + text
+    message.extend([
+      MessageSegment.text(text + f"\n{URL_BASE}{game.slug}\n"),
+      MessageSegment.image(game.image)
+    ])
+  await epicfree.finish(Message(message))
