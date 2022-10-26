0e5cb3898469e089f74acd8a56e0cc0907d6620e
su226 <thesu226@outlook.com>
2022-10-26 17:46:52 +0800

    修复部分平台无法读取A卡占用的bug

--- plugins/idhagn_fetch/gpu/amd.py
+++ plugins/idhagn_fetch/gpu/amd.py
@@ -1,3 +1,4 @@
+import os
 import re
 import subprocess as sp
 
@@ -5,12 +6,12 @@ from .common import Info
 
 PERCENT_FILE = "gpu_busy_percent"
 MEM_PERCENT_FILE = "mem_busy_percent"
-CLK_FILE = "hwmon/hwmon2/freq1_input"
-MEM_CLK_FILE = "hwmon/hwmon2/freq2_input"
-TEMP_FILE = "hwmon/hwmon2/temp1_input"
-JUNCTION_TEMP_FILE = "hwmon/hwmon2/temp2_input"
-MEM_TEMP_FILE = "hwmon/hwmon2/temp3_input"
-VDD_FILE = "hwmon/hwmon2/in0_input"
+CLK_FILE = "freq1_input"
+MEM_CLK_FILE = "freq2_input"
+TEMP_FILE = "temp1_input"
+JUNCTION_TEMP_FILE = "temp2_input"
+MEM_TEMP_FILE = "temp3_input"
+VDD_FILE = "in0_input"
 UEVENT_FILE = "uevent"
 
 
@@ -29,12 +30,13 @@ def read(card: str) -> Info:
   model = model[model.rfind("[") + 1:model.rfind("]")]
   model = model.replace("OEM", "")
   model = re.sub(r"\s*/\s*", "/", model)
+  hwmon = root + "hwmon/" + os.listdir(root + "hwmon")[0] + "/"
   with open(root + PERCENT_FILE) as f:
     percent = int(f.read())
   with open(root + MEM_PERCENT_FILE) as f:
     mem_percent = int(f.read())
-  with open(root + CLK_FILE) as f:
+  with open(hwmon + CLK_FILE) as f:
     clk = int(f.read())
-  with open(root + TEMP_FILE) as f:
+  with open(hwmon + TEMP_FILE) as f:
     temp = int(f.read()) // 1000
   return Info(False, "AMD/ATI " + model, percent, mem_percent, clk, temp)
