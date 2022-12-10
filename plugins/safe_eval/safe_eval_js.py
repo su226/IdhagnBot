02f3e262f081e05dfb5dd6de26489a243d2c8ebe
su226 <thesu226@outlook.com>
2022-12-10 18:34:10 +0800

    修复 save_eval_js 的依赖问题

--- plugins/safe_eval/safe_eval_js.py
+++ plugins/safe_eval/safe_eval_js.py
@@ -19,22 +19,24 @@ plugin_dir = os.path.dirname(os.path.abspath(__file__))
 async def safe_eval(
   code: str, timeout: float, nproc: int, memory: int, output: int
 ) -> Tuple[bool, int, bytes, bytes]:
+  # TODO: 得有个办法自动获取这个
   proc = await asyncio.subprocess.create_subprocess_exec(
     "bwrap",
     "--unshare-all",
     "--clearenv",
     "--die-with-parent",
     "--ro-bind", "/lib64/ld-linux-x86-64.so.2", "/lib64/ld-linux-x86-64.so.2",
+    "--ro-bind", "/usr/lib/libnode.so.111", "/usr/lib/libnode.so.111",
     "--ro-bind", "/usr/lib/libz.so.1", "/usr/lib/libz.so.1",
     "--ro-bind", "/usr/lib/libuv.so.1", "/usr/lib/libuv.so.1",
     "--ro-bind", "/usr/lib/libbrotlidec.so.1", "/usr/lib/libbrotlidec.so.1",
     "--ro-bind", "/usr/lib/libbrotlienc.so.1", "/usr/lib/libbrotlienc.so.1",
     "--ro-bind", "/usr/lib/libcares.so.2", "/usr/lib/libcares.so.2",
     "--ro-bind", "/usr/lib/libnghttp2.so.14", "/usr/lib/libnghttp2.so.14",
-    "--ro-bind", "/usr/lib/libcrypto.so.1.1", "/usr/lib/libcrypto.so.1.1",
-    "--ro-bind", "/usr/lib/libssl.so.1.1", "/usr/lib/libssl.so.1.1",
-    "--ro-bind", "/usr/lib/libicui18n.so.71", "/usr/lib/libicui18n.so.71",
-    "--ro-bind", "/usr/lib/libicuuc.so.71", "/usr/lib/libicuuc.so.71",
+    "--ro-bind", "/usr/lib/libcrypto.so.3", "/usr/lib/libcrypto.so.3",
+    "--ro-bind", "/usr/lib/libssl.so.3", "/usr/lib/libssl.so.3",
+    "--ro-bind", "/usr/lib/libicui18n.so.72", "/usr/lib/libicui18n.so.72",
+    "--ro-bind", "/usr/lib/libicuuc.so.72", "/usr/lib/libicuuc.so.72",
     "--ro-bind", "/usr/lib/libstdc++.so.6", "/usr/lib/libstdc++.so.6",
     "--ro-bind", "/usr/lib/libm.so.6", "/usr/lib/libm.so.6",
     "--ro-bind", "/usr/lib/libgcc_s.so.1", "/usr/lib/libgcc_s.so.1",
@@ -42,7 +44,7 @@ async def safe_eval(
     "--ro-bind", "/usr/lib/libdl.so.2", "/usr/lib/libdl.so.2",
     "--ro-bind", "/usr/lib/libpthread.so.0", "/usr/lib/libpthread.so.0",
     "--ro-bind", "/usr/lib/libbrotlicommon.so.1", "/usr/lib/libbrotlicommon.so.1",
-    "--ro-bind", "/usr/lib/libicudata.so.71", "/usr/lib/libicudata.so.71",
+    "--ro-bind", "/usr/lib/libicudata.so.72", "/usr/lib/libicudata.so.72",
     "--ro-bind", "/usr/bin/node", "/usr/bin/node",
     "--ro-bind", "states/safe_eval_js/node_modules", "/node_modules",
     "--ro-bind", os.path.join(plugin_dir, "do_eval.js"), "/do_eval.js",
