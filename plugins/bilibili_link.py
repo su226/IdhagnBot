d9c4a750ef67c10dbfae10d0fad34325b5c1fe97
su226 <thesu226@outlook.com>
2023-07-29 19:39:59 +0800

    feat(bilibili_link): 在消息中有多个链接时警告

--- plugins/bilibili_link.py
+++ plugins/bilibili_link.py
@@ -2,10 +2,10 @@ import json
 import re
 import time
 from io import BytesIO
-from typing import Dict, Tuple
+from typing import Any, Dict, Generator, Tuple
 
 import nonebot
-from nonebot.adapters.onebot.v11 import Message, MessageSegment, MessageEvent, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message, MessageEvent, MessageSegment
 from nonebot.params import EventMessage
 from nonebot.typing import T_State
 from PIL import Image
@@ -16,7 +16,8 @@ from util.images.card import Card, CardAuthor, CardCover, CardInfo, CardText, In
 VIDEO_RE = re.compile(r"av\d{1,9}|(BV|bv)[A-Za-z0-9]{10}")
 LINK_RE = re.compile(
   r"bilibili\.com/video/(av\d{1,9}|(BV|bv)[A-Za-z0-9]{10})"
-  r"|b23\.tv/(av\d{1,9}|BV[A-Za-z0-9]{10}|[A-Za-z0-9]{7})")
+  r"|b23\.tv/(av\d{1,9}|BV[A-Za-z0-9]{10}|[A-Za-z0-9]{7})"
+)
 INFO_API = "https://api.bilibili.com/x/web-interface/view/detail"
 ParseRequest = Tuple[str, str, int]
 last_videos: Dict[int, ParseRequest] = {}
@@ -29,26 +30,85 @@ def is_link_alike(last: ParseRequest, link: str) -> bool:
   判定为解析请求相似，避免再次发送结果
   '''
   last_link, last_bvid, last_aid = last
-  return last_link == link or last_bvid in link or str(last_aid) in link
-
-
-async def check_bilibili_link(state: T_State, msg: Message = EventMessage()) -> bool:
-  if msg[0].type == "json":
-    data = json.loads(msg[0].data["data"])
-    try:
-      url = data["meta"]["detail_1"]["qqdocurl"]
-    except KeyError:
-      try:
-        url = data["meta"]["news"]["jumpUrl"]
-      except KeyError:
-        return False
-    if match := LINK_RE.search(url):
-      state["link"] = match[0]
-      return True
-    return False
-  if match := LINK_RE.search(msg.extract_plain_text()):
-    state["link"] = match[0]
+  if last_link == link:
     return True
+  if match := VIDEO_RE.search(link):
+    video = match[0]
+    if video[:2] == "av":
+      return int(video[2:]) == last_aid
+    else:
+      return video[2:] == last_bvid
+  return False
+
+
+def extract_bilibili_links(msg: Message) -> Generator[str, Any, Any]:
+  '''提取消息中所有的可能是 B 站视频的链接，视频不一定有效，提取出的 b23.tv 短链也不一定指向视频'''
+  for seg in msg:
+    if seg.type == "json":
+      data = json.loads(seg.data["data"])
+      try:
+        url = data["meta"]["detail_1"]["qqdocurl"]
+      except KeyError:
+        try:
+          url = data["meta"]["news"]["jumpUrl"]
+        except KeyError:
+          continue
+      if match := LINK_RE.search(url):
+        yield match[0]
+    elif seg.type == "text":
+      yield from (match[0] for match in LINK_RE.finditer(msg.extract_plain_text()))
+
+
+async def check_bilibili_link(
+  event: MessageEvent, state: T_State, msg: Message = EventMessage()
+) -> bool:
+  http = misc.http()
+  first_valid = None
+  link_count = 0
+  if isinstance(event, GroupMessageEvent):
+    last_video = last_videos.get(event.group_id, None)
+  else:
+    last_video = None
+
+  for link in extract_bilibili_links(msg):
+    # 不包括上一次发送过的链接，防止群内有多个机器人时刷屏
+    if last_video and is_link_alike(last_video, link):
+      continue
+    link_count += 1
+    # 此处只是为了验证是否有多于一个链接，因此如果已有视频数据则只统计数量
+    if first_valid is not None:
+      if link_count > 1:
+        break
+      continue
+
+    if match := VIDEO_RE.search(link):
+      video = match[0]
+    else:
+      # 获取真实链接（如果是b23.tv短链接）
+      async with http.get("https://" + link, allow_redirects=False) as response:
+        if match := VIDEO_RE.search(response.headers.get("Location", "")):
+          video = match[0]
+        else:
+          continue
+
+    if video[:2] == "av":
+      params = {"aid": video[2:]}
+    else:
+      params = {"bvid": video}
+    async with http.get(INFO_API, params=params) as response:
+      data = await response.json()
+    # 没有 data 说明视频失效
+    if "data" in data:
+      first_valid = link, data["data"]
+
+  if first_valid is not None:
+    link, data = first_valid
+    if isinstance(event, GroupMessageEvent):
+      last_videos[event.group_id] = (link, data["View"]["bvid"][2:], data["View"]["aid"])
+    state["data"] = data
+    state["more"] = link_count > 1
+    return True
+
   return False
 
 bilibili_link = nonebot.on_message(
@@ -60,38 +120,11 @@ bilibili_link = nonebot.on_message(
 async def handle_bilibili_link(event: MessageEvent, state: T_State) -> None:
   http = misc.http()
 
-  # 1. 获取真实链接（如果是b23.tv短链接）
-  link = state["link"]
-  match = VIDEO_RE.search(link)
-  if not match:
-    async with http.get("https://" + link, allow_redirects=False) as response:
-      match = VIDEO_RE.search(response.headers.get("Location", ""))
-      if not match:
-        return
-
-  # 2. 获取视频信息、封面、UP主头像
-  video = match[0]
-  if video[:2].lower() == "bv":
-    url = INFO_API + "?bvid=" + video
-  else:
-    url = INFO_API + "?aid=" + video[2:]
-  async with http.get(url) as response:
-    data = await response.json()
-  if "data" not in data:
-    return
-
-  data_view = data["data"]["View"]
-  data_card = data["data"]["Card"]["card"]
+  # 1. 获取视频信息、封面、UP主头像
+  data = state["data"]
+  data_view = data["View"]
+  data_card = data["Card"]["card"]
   data_stat = data_view["stat"]
-  bvid = misc.removeprefix(data_view["bvid"], "BV")
-  aid: int = data_view["aid"]
-
-  if isinstance(event, GroupMessageEvent):
-    # 如果上一次解析的链接相似，放弃发送，防止群内有多个机器人时刷屏
-    last_video = last_videos.get(event.group_id, None)
-    if last_video and is_link_alike(last_video, link):
-      await bilibili_link.finish()
-    last_videos[event.group_id] = (link, bvid, aid)
 
   async with http.get(data_card["face"]) as response:
     avatar_data = await response.read()
@@ -99,7 +132,7 @@ async def handle_bilibili_link(event: MessageEvent, state: T_State) -> None:
     cover_data = await response.read()
 
   def make() -> MessageSegment:
-    # 3. 构建卡片
+    # 2. 构建卡片
     card = Card(0)
 
     block = Card()
@@ -131,10 +164,12 @@ async def handle_bilibili_link(event: MessageEvent, state: T_State) -> None:
       block.add(CardText(desc, 32, 3))
     card.add(block)
 
-    # 4. 渲染卡片并发送
+    # 3. 渲染卡片并发送
     im = Image.new("RGB", (card.get_width(), card.get_height()), (255, 255, 255))
     card.render(im, 0, 0)
     return imutil.to_segment(im)
 
-  info = f"https://www.bilibili.com/video/BV{bvid} (av{aid})"
-  await bilibili_link.finish(info + await misc.to_thread(make), reply_message=True)
+  info = f"https://www.bilibili.com/video/{data_view['bvid']} (av{data_view['aid']})"
+  if state["more"]:
+    info += "\n⚠发现多个链接，结果仅包含第一个有效视频"
+  await bilibili_link.finish(await misc.to_thread(make) + info)
