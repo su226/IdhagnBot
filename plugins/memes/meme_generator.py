c7eb9cd587375cfbe3ef75e03bf2503b594e0973
su226 <thesu226@outlook.com>
2025-04-22 22:02:58 +0800

    fix(memes): 适配 meme-generator-rs

--- plugins/memes/meme_generator.py
+++ plugins/memes/meme_generator.py
@@ -4,9 +4,12 @@ from io import BytesIO
 from typing import Any, Optional
 
 import nonebot
-from meme_generator import Meme, get_memes
-from meme_generator.download import check_resources
-from meme_generator.meme import UserInfo
+from meme_generator import (
+  BooleanOption, DeserializeError, FloatOption, Image as MemeImage, ImageAssetMissing,
+  ImageDecodeError, ImageEncodeError, ImageNumberMismatch, Meme, MemeFeedback, StringOption,
+  TextNumberMismatch, TextOverLength, get_memes,
+)
+from meme_generator.resources import check_resources_in_background
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
 from nonebot.params import ShellCommandArgs
 from nonebot.rule import ArgumentParser
@@ -18,8 +21,8 @@ driver = nonebot.get_driver()
 
 
 @driver.on_startup
-async def _() -> None:
-  asyncio.create_task(check_resources())
+def _() -> None:
+  check_resources_in_background()
 
 
 async def get_avatar_and_info_from_pattern(
@@ -34,22 +37,22 @@ async def get_avatar_and_info_from_id(bot: Bot, uid: int) -> tuple[Image.Image, 
 
 
 async def check_image_count(meme: Meme, count: int) -> None:
-  if not (meme.params_type.min_images <= count <= meme.params_type.max_images):
+  if not (meme.info.params.min_images <= count <= meme.info.params.max_images):
     raise misc.AggregateError(
-      f"输入图片数量不符，图片数量应为 {meme.params_type.min_images}" + (
-        f" ~ {meme.params_type.max_images}"
-        if meme.params_type.max_images > meme.params_type.min_images
+      f"输入图片数量不符，图片数量应为 {meme.info.params.min_images}" + (
+        f" ~ {meme.info.params.max_images}"
+        if meme.info.params.max_images > meme.info.params.min_images
         else ""
       ),
     )
 
 
 async def check_text_count(meme: Meme, count: int) -> None:
-  if not (meme.params_type.min_texts <= count <= meme.params_type.max_texts):
+  if not (meme.info.params.min_texts <= count <= meme.info.params.max_texts):
     raise misc.AggregateError(
-      f"输入文字数量不符，文字数量应为 {meme.params_type.min_texts}" + (
-        f" ~ {meme.params_type.max_texts}"
-        if meme.params_type.max_texts > meme.params_type.min_texts
+      f"输入文字数量不符，文字数量应为 {meme.info.params.min_texts}" + (
+        f" ~ {meme.info.params.max_texts}"
+        if meme.info.params.max_texts > meme.info.params.min_texts
         else ""
       ),
     )
@@ -58,27 +61,46 @@ async def check_text_count(meme: Meme, count: int) -> None:
 def create_matcher(meme: Meme) -> None:
   parser = ArgumentParser(add_help=False)
   parser.add_argument("meme_params", nargs="*")
-  if meme.params_type.args_type:
-    for option in meme.params_type.args_type.parser_options:
-      if option.args:
-        parser.add_argument(
-          *(name for name in option.names if name.startswith("-")),
-          dest=option.args[0].name,
-          default=option.args[0].default,
-          help=option.help_text,
-        )
-      else:
-        parser.add_argument(
-          *(name for name in option.names if name.startswith("-")),
-          dest=option.dest,
-          default=option.default,
-          action="store_const",
-          const=option.action.value if option.action else None,
-          help=option.help_text,
-        )
+  for option in meme.info.params.options:
+    names = []
+    if option.parser_flags.short:
+      names.append(f"-{option.name[0]}")
+    names.extend(f"-{x}" for x in option.parser_flags.short_aliases)
+    if option.parser_flags.long:
+      names.append(f"--{option.name}")
+    names.extend(f"--{x}" for x in option.parser_flags.long_aliases)
+    if isinstance(option, BooleanOption):
+      if option.default is None:
+        print(meme, option)
+      parser.add_argument(
+        *names,
+        action="store_false" if option.default else "store_true",
+        dest=option.name,
+        help=option.description,
+      )
+    elif isinstance(option, StringOption):
+      parser.add_argument(
+        *names,
+        default=option.default,
+        choices=option.choices,
+        help=option.description,
+        dest=option.name,
+      )
+    else:
+      parser.add_argument(
+        *names,
+        default=option.default,
+        type=(
+          misc.range_float(option.minimum, option.maximum)
+          if isinstance(option, FloatOption) else
+          misc.range_int(option.minimum, option.maximum) 
+        ),
+        help=option.description,
+        dest=option.name,
+      )
 
   matcher = (
-    command.CommandBuilder(f"memes.{meme.key}", *meme.keywords)
+    command.CommandBuilder(f"memes.{meme.key}", *meme.info.keywords)
     .category("memes")
     .shell(parser)
     .build()
@@ -91,6 +113,7 @@ def create_matcher(meme: Meme) -> None:
   ):
     tasks: list[asyncio.Task[tuple[Image.Image, Optional[dict[str, Any]]]]] = []
     texts: list[str] = []
+    names: list[str] = []
 
     async with user_aliases.AvatarGetter(bot, event) as g:
       for arg in ns.meme_params:
@@ -104,11 +127,13 @@ def create_matcher(meme: Meme) -> None:
             "他", "她", "牠", "祂",
           }
         ):
-          tasks.append(g.submit(get_avatar_and_info_from_pattern(g, arg)))
+          tasks.append(g.submit(get_avatar_and_info_from_pattern(g, arg.removeprefix("@"))))
+        elif arg.startswith("#"):
+          names.append(arg[1:])
         elif arg:
           texts.append(arg)
 
-      if meme.params_type.min_images == 2:
+      if meme.info.params.min_images == 2:
         if len(tasks) == 1:
           # 当所需图片数为 2 且已指定图片数为 1 时，使用发送者的头像作为第一张图
           tasks.insert(0, g.submit(get_avatar_and_info_from_id(bot, event.user_id)))
@@ -116,31 +141,61 @@ def create_matcher(meme: Meme) -> None:
           # 当所需图片数为 2 且没有已指定图片时，使用发送者和机器人的头像
           tasks.append(g.submit(get_avatar_and_info_from_id(bot, event.user_id)))
           tasks.append(g.submit(get_avatar_and_info_from_id(bot, event.self_id)))
-      elif meme.params_type.min_images == 1:
+      elif meme.info.params.min_images == 1:
         if len(tasks) == 0:
           # 当所需图片数为 1 且没有已指定图片时，使用发送者的头像
           tasks.append(g.submit(get_avatar_and_info_from_id(bot, event.user_id)))
 
-      if meme.params_type.min_texts > 0 and len(texts) == 0:
+      if meme.info.params.min_texts > 0 and len(texts) == 0:
         # 当所需文字数 > 0 且没有输入文字时，使用默认文字
-        texts = meme.params_type.default_texts
+        texts = meme.info.params.default_texts
 
       g.submit(check_image_count(meme, len(tasks)))
       g.submit(check_text_count(meme, len(texts)))
 
     def make() -> MessageSegment:
-      images_data: list[BytesIO] = []
-      users: list[UserInfo] = []
-      for task in tasks:
+      images: list[MemeImage] = []
+      for i, task in enumerate(tasks):
         image, user = task.result()
         f = BytesIO()
         image.save(f, "png")
-        images_data.append(f)
-        if user:
-          users.append(UserInfo(name=user["nickname"], gender=user["sex"]))
-      args = {k: v for k, v in ns.__dict__.items() if k != "meme_params"}
-      args["user_infos"] = users
-      return MessageSegment.image(meme(images=images_data, texts=texts, args=args))
+        name_override = names[i] if i < len(names) else ""
+        if name_override:
+          images.append(MemeImage(name_override, f.getvalue()))
+        elif user:
+          images.append(MemeImage(user["nickname"], f.getvalue()))
+        else:
+          images.append(MemeImage("", f.getvalue()))
+      options = {k: v for k, v in ns.__dict__.items() if k != "meme_params" and v is not None}
+      result = meme.generate(images, texts, options)
+      if isinstance(result, ImageDecodeError):
+        return MessageSegment.text(f"图片解码出错：{result.error}")
+      elif isinstance(result, ImageEncodeError):
+        return MessageSegment.text(f"图片编码出错：{result.error}")
+      elif isinstance(result, ImageAssetMissing):
+        return MessageSegment.text(f"缺少图片资源：{result.path}")
+      elif isinstance(result, DeserializeError):
+        return MessageSegment.text(f"表情选项解析出错：{result.error}")
+      elif isinstance(result, ImageNumberMismatch):
+        num = (
+          f"{result.min} ~ {result.max}"
+          if result.min != result.max
+          else str(result.min)
+        )
+        return MessageSegment.text(f"图片数量不符，应为 {num}，实际传入 {result.actual}")
+      elif isinstance(result, TextNumberMismatch):
+        num = (
+          f"{result.min} ~ {result.max}"
+          if result.min != result.max
+          else str(result.min)
+        )
+        return MessageSegment.text(f"文字数量不符，应为 {num}，实际传入 {result.actual}")
+      elif isinstance(result, TextOverLength):
+        repr = result.text[:10] + "..." if len(result.text) > 10 else result.text
+        return MessageSegment.text(f"文字过长：{repr}")
+      elif isinstance(result, MemeFeedback):
+        return MessageSegment.text(result.feedback)
+      return MessageSegment.image(result)
 
     await matcher.finish(await misc.to_thread(make))
 
